cf55143d30d65cb552bb621fb23688c0368991f3
Keiko M <vikakeiko21@gmail.com>
2019-06-13 16:53:15 -0700

    halfway

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,30 +1,68 @@
 var win = 0;
 var lose = 0;
-var remaningGuesses = 9;
+var remaningGuesses = 10;
 var pickedGuess = [];
+var winDisplay = document.getElementById("win");
+var loseDisplay = document.getElementById("lose");
+var remaningGuess = document.getElementById("remainingGuesses");
+var displayPicked = document.getElementById("picked");
 
-var options = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var choice = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var cpuGuess = choice[Math.floor(Math.random() * choice.length)];
 
-// create random choice for computer 
-var cpuGuess = options[Math.floor(Math.random() * options.length)];
-var numberOfTries = 0;
-
+// var pickLetter = prompt("Pick a letter from \"a\" to \"z\"?");
 
 document.onkeyup = function (event) {
-    // user guess 
+
+    // user guess which key was pressed 
     var userGuess = event.key;
-    console.log(event.key);
-    console.log(cpuGuess);
 
+    pickedGuess.push(userGuess);
+    displayPicked.textContent = pickedGuess;
+    // create random choice for computer 
+    console.log("choice", choice);
+    // var cpuGuess = options[Math.floor(Math.random() * options.length)];
+    // userGuess = cpuGuess;
+    console.log("User Guess: " + userGuess);
+    console.log("Computer Guess " + cpuGuess);
 
-    if (userGuess === cpuGuess) {
-        // user win 
-        win++;
+    if (choice.indexOf(userGuess) > -1) {
+
+        if (userGuess === cpuGuess) {
+            win++;
+            alert("You guessed right!");
+            winDisplay.textContent = win;
+        }
+        if (userGuess !== cpuGuess) {
+            lose++;
+            alert("WRONGGG!");
+            pickedGuess.push(userGuess);
+            loseDisplay.textContent = lose;
+            remaningGuesses--;
+        }
+        if(remaningGuesses < 0){
+        alert("game over!");
     }
+    remaningGuess.innerText = remaningGuesses;
+    } 
 
-    if (userGuess !== cpuGuess) {
-        // user lose 
-        lose++;
-    }
+
+
+    //     if (userGuess === cpuGuess) {
+    //         win++;
+    //         alert("You guessed right!");
+    //         winDisplay.textContent = win;
+    //     } else {
+    //         lose++;
+    //         alert("WRONGGG!");
+    //         pickedGuess.push(userGuess);
+    //         loseDisplay.textContent = lose;
+    //         remaningGuesses--;
+    //     }
+    //   remaningGuess.innerText = " Guesses Remaining: " + remaningGuesses;
+
 
 }
+
+// winDisplay.innerText = " Wins: " + win;
+// loseDisplay.innerText = " Losses: " + lose;
